56e72ea85279ff13a836c611b913b9763073e2b5
MiniAlbum <d18785554501@163.com>
2018-09-18 20:15:53 +0800

    PHP

--- php.php
+++ php.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
 <?php 
   $arr_test =
 array(1, 2, 3);
- print_r($arr_test);
+ echo array;
 ?>
